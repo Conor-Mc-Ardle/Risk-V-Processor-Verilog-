78a3071c67650cdb36efa1395f873e40c2a73256
Hkmm07 <mcardleconor.work@gmail.com>
2026-06-22 03:14:49 +0100

    Added a couple of new test cases to testbench, note i discovered i didnt need to compare to expected e in if statement, however i will revert this as it makes the fail cases in the dump.vcd more difficult to read. Its 3am i am going to bed, remember to fix tmr

--- Risc_V_Proccessor/Register_File/Register_File_Tb.v
+++ Risc_V_Proccessor/Register_File/Register_File_Tb.v
@@ -19,8 +19,41 @@ initial begin
 
     clk = 0;
 
-// Write 42 to x1
+// Write 42 to x1 then read back
 rd = 5'd1; write_data = 32'd42; reg_write = 1;
+rs1 = 5'd1; rs2 = 5'd0;
+@(posedge clk); #1;
+
+if (read_data1 == 32'd42 && read_data2 == 32'd0)
+    $display("Pass");
+else
+    $display("Fail! Result/Expected - Read_Data1:%0d/%0d - Read_Data2:%0d/%0d",
+    read_data1, e_read_data1, read_data2, e_read_data2);
+
+// Write 100 to x2 then read back
+rd = 5'd2; write_data = 32'd100; reg_write = 1;
+rs1 = 5'd0; rs2 = 5'd2;
+@(posedge clk); #1;
+
+if (read_data1 == 32'd0 && read_data2 == 32'd100)
+    $display("Pass");
+else
+    $display("Fail! Result/Expected - Read_Data1:%0d/%0d - Read_Data2:%0d/%0d",
+    read_data1, e_read_data1, read_data2, e_read_data2);
+
+// Write to x0 then read back, x0 is hardwired to 0 so should remain as 0
+rd = 5'd0; write_data = 32'd999; reg_write = 1;
+rs1 = 5'd0; rs2 = 5'd0;
+@(posedge clk); #1;
+
+if (read_data1 == 32'd0 && read_data2 == 32'd0)
+    $display("Pass");
+else
+    $display("Fail! Result/Expected - Read_Data1:%0d/%0d - Read_Data2:%0d/%0d",
+    read_data1, e_read_data1, read_data2, e_read_data2);
+
+// reg_write set to 0 so it shouldnt write. It should maintain its previous value (42) and not the new value 365
+rd = 5'd1; write_data = 32'd365; reg_write = 0;
 rs1 = 5'd1; rs2 = 5'd0;
 @(posedge clk); #1;
 
